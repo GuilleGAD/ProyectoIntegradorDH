515c6a2d968b4c89cb50ecf8957c8fa1ef6082bc
Guillermo A. Diarte <guillermo.diarte@gmail.com>
2019-12-03 13:49:04 -0300

    reestructuramiento

---
+++ register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- HEAD -->
+    <?php require_once("phpCompartido/head.php");?>
+  <body>
+    <!-- HEADER -->
+    <?php require_once("phpCompartido/header.php");?>
+
+    <div class="imagenConstruccion"></div>
+
+    <!-- FOOTER -->
+    <?php require_once("phpCompartido/footer.php");?>
+  </body>
+</html>
